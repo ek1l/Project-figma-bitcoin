6192859c6d7d428dfacea0a9a0f64b20b038c91d
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-05-22 06:36:55 -0300

    terminei o site

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ let pegarButton = document.querySelector('.bn47')
 
 
 let pegarNav = document.querySelector('nav') 
-
+let moverButton = document.querySelector(".moverbutao")
+let pegarHeader = document.querySelector('header')
 pegarButton.addEventListener('click', () => {
    pegarNav.classList.toggle('active')
+   pegarHeader.classList.toggle('active')
+   moverButton.classList.toggle('active')
+
+
 })
