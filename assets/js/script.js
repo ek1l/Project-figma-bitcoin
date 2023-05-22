f170a86e69aae882c69efae9b93f07d65be201a8
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-05-22 05:20:21 -0300

    add menu hamburguer

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let pegarButton = document.querySelector('.bn47')
+
+
+let pegarNav = document.querySelector('nav') 
+
+pegarButton.addEventListener('click', () => {
+   pegarNav.classList.toggle('active')
+})
